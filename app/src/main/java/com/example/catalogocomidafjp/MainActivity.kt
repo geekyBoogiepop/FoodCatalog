4bd919bbbc03d9cddacc6301e959a23adee3ea6e
geekyBoogiepop <fljustsocial@outlook.com>
2023-06-12 16:43:03 -0600

    feat: Add blank Home, Favorites and Categories fragments

--- app/src/main/java/com/example/catalogocomidafjp/MainActivity.kt
+++ app/src/main/java/com/example/catalogocomidafjp/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.catalogocomidafjp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import com.example.catalogocomidafjp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val bottomNavigation = binding.bottomNavigation
+        val navController = Navigation.findNavController(this, R.id.main_fragment)
+
+        NavigationUI.setupWithNavController(bottomNavigation, navController)
     }
 }
